9e44b38bee29974728d3efdfbcc05df2f305cc06
Jacob Flohre <jake@heysparkbox.com>
2013-06-27 00:37:17 -0400

    Starting rspec testing

---
+++ app/controllers/triangles_controller.rb
@@ -0,0 +1,4 @@
+class TrianglesController < ApplicationController
+  def is_real_triangle?(side1, side2, side3)
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TriangleKata::Application.config.secret_token = 'ca212b5b42eb31618064b00b35a5e9a79e5a55e3b1364fa52a057de81097c4d1157be8e515918bcf75b6a1cd07a9512e65e15ef6f48ac42b7fc135ebd8f0d341'
